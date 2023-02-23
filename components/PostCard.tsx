44b48ebc7cb44c8e57f030e22fca7aa58a81981c
Kaio Silveira <kaionpsa@gmail.com>
2023-02-23 18:00:33 -0300

    postcard

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -3,7 +3,24 @@ import moment from 'moment';
 import Link from 'next/link';
 
 interface PostProps {
-    post: any
+    post: {
+      author: {
+        bio?: string;
+        id: string;
+        name: string;
+        photo?: {
+          url: string;
+        }
+      };
+      categories?: object;
+      createdAt: string;
+      excerpt: string;
+      featuredImage: {
+        url: string;
+      },
+      slug: string;
+      title: string;
+    }
 }
 
 const PostCard: FC<PostProps> = ({post}) => {
@@ -18,6 +35,36 @@ const PostCard: FC<PostProps> = ({post}) => {
         src={post?.featuredImage?.url} 
         alt={post.title} />
       </div>
+
+      <h1 className="transition duration-400 text-center mb-8 cursor:pointer hover:text-pink-600 text-3xl font-semibold">
+        <Link href={`/post/${post.slug}`}>
+          {post.title}
+        </Link>
+      </h1>
+
+      <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
+        <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
+          <img 
+          className="align-middle rounded-full h-[30px] w-[30px]"
+          src={post.author.photo?.url} 
+          alt={post.author.name} />
+          <p className="inline align-middle text-gray-700 ml-2 text-lg">{post.author.name}</p>
+        </div>
+        <div className="font-medium text-gray-700">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+            </svg>
+            <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
+        </div>
+      </div>
+
+      <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-4">{post.excerpt}</p>
+      <div className="text-center">
+        <Link href={`/post/${post.slug}`}>
+          <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Continue Reading</span>
+        </Link>
+      </div>
+
     </div>
   )
 }
